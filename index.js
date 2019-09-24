7a6319be78363dd144428d2dffae964c0459db7f
CallumReynolds <45584439+CallumReynolds@users.noreply.github.com>
2019-09-24 12:00:10 +1000

    add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// First Number and Dice Generator
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+// Second Number and Dice Generator
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// Checks to see which number wins
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🎈 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🎈";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
